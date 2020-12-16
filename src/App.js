72e92c7a8b99c958dcaae45c02c3a0701166cd1c
toycrane <alwaysfun2183@gmail.com>
2020-12-16 16:10:36 +0900

    App.js 구조 잡기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
 import "./App.css";
+import React from "react";
 
 function App() {
 	return (
-		<div>
-			나의 첫 리액트 프로젝트
-			<button>버튼</button>
+		<div className="container">
+			<div className="app">
+				<div className="question-section">
+					<h1 className="question-header">
+						<span>1</span>/4
+					</h1>
+					<div className="question-text">
+						일론 머스크의 우주 탐사 기업 이름은?
+					</div>
+				</div>
+				<div className="answer-section">
+					<button>스페이스 엑스</button>
+					<button>테슬라</button>
+					<button>보링 컴퍼니</button>
+					<button>솔라시티</button>
+				</div>
+			</div>
 		</div>
 	);
 }
