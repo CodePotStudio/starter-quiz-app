8cf227706d265c9bc6a67cf94ebfc99fda689a7e
toycrane <alwaysfun2183@gmail.com>
2021-02-15 15:29:49 +0900

    정답, 오답 코드 작성 완료

--- src/App.js
+++ src/App.js
@@ -2,10 +2,41 @@ import React from "react";
 import "./App.css";
 
 function App() {
+	const handleClick = (e) => {
+		const answer = e.target.value;
+		if (answer === "스페이스 엑스") {
+			alert("정답 👏👏👏");
+		} else {
+			alert("오답 😣😣😣");
+		}
+	};
+
 	return (
-		<div>
-			<div>Hello React!</div>
-			<button>button</button>
+		<div className="container">
+			<div className="app">
+				<div className="question-section">
+					<h1 className="question-header">
+						<span>1</span>/4
+					</h1>
+					<div className="question-text">
+						일론 머스크의 우주 탐사 기업 이름은?
+					</div>
+				</div>
+				<div className="answer-section">
+					<button onClick={handleClick} value="스페이스 엑스">
+						스페이스 엑스
+					</button>
+					<button onClick={handleClick} value="테슬라">
+						테슬라
+					</button>
+					<button onClick={handleClick} value="보링 컴퍼니">
+						보링 컴퍼니
+					</button>
+					<button onClick={handleClick} value="솔라시티">
+						솔라시티
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
