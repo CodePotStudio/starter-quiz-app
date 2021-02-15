a70b3c738409bc71ff6e879df0d76a3be162dc29
toycrane <alwaysfun2183@gmail.com>
2021-02-15 16:15:16 +0900

    1주차 3번째 자료 작성

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import React from "react";
-import "./App.css";
 
 function App() {
 	return (
-		<div className="container">
-			<div className="app">
-				<div className="question-section">
-					<h1 className="question-header">
-						<span>1</span>/4
-					</h1>
-					<div className="question-text">
-						일론 머스크의 우주 탐사 기업 이름은?
-					</div>
-				</div>
-				<div className="answer-section">
-					<button>스페이스 엑스</button>
-					<button>테슬라</button>
-					<button>보링 컴퍼니</button>
-					<button>솔라시티</button>
-				</div>
-			</div>
+		<div>
+			<div>Hello React!</div>
+			<button>button</button>
 		</div>
 	);
 }
