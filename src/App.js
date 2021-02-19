cceb659e3e821bf01ae0a3c1e4ad9481933850fd
toycrane <alwaysfun2183@gmail.com>
2021-02-20 00:49:19 +0900

    4번 코드 업데이트

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-	const quiz = {
-		question: "일론 머스크의 우주 탐사 기업 이름은?",
-		answers: [
-			{ text: "스페이스 엑스", isCorrect: true },
-			{ text: "테슬라", isCorrect: false },
-			{ text: "보링 컴퍼니", isCorrect: false },
-			{ text: "솔라시티", isCorrect: false },
-		],
-	};
+	const [currentNo, setCurrentNo] = useState(0);
+	const quizzes = [
+		{
+			id: 1,
+			question: "일론 머스크의 우주 탐사 기업 이름은?",
+			answers: [
+				{ text: "스페이스 엑스", isCorrect: true },
+				{ text: "테슬라", isCorrect: false },
+				{ text: "보링 컴퍼니", isCorrect: false },
+				{ text: "솔라시티", isCorrect: false },
+			],
+		},
+		{
+			id: 2,
+			question: "일론 머스크의 고향은 어디일까요?",
+			answers: [
+				{ text: "미국 캘리포니아", isCorrect: false },
+				{ text: "남아프리카 공화국 프리토리아", isCorrect: true },
+				{ text: "캐나다 벤쿠버", isCorrect: false },
+				{ text: "호주 시드니", isCorrect: false },
+			],
+		},
+		{
+			id: 3,
+			question: "일론 머스크가 창업한 페이팔 전신 기업의 이름은?",
+			answers: [
+				{ text: "Zip2 Corporation", isCorrect: false },
+				{ text: "Alpha Exploration co.", isCorrect: false },
+				{ text: "X.com", isCorrect: true },
+				{ text: "Everything CO.", isCorrect: false },
+			],
+		},
+	];
 
 	const handleClick = (isCorrect) => {
 		if (isCorrect) {
@@ -18,6 +42,7 @@ function App() {
 		} else {
 			alert("오답 😣😣😣");
 		}
+		setCurrentNo((currentNo) => currentNo + 1);
 	};
 
 	return (
@@ -25,12 +50,12 @@ function App() {
 			<div className="app">
 				<div className="question-section">
 					<h1 className="question-header">
-						<span>1</span>/4
+						<span>{quizzes[currentNo].id}</span>/{quizzes.length}
 					</h1>
-					<div className="question-text">{quiz.question}</div>
+					<div className="question-text">{quizzes[currentNo].question}</div>
 				</div>
 				<div className="answer-section">
-					{quiz.answers.map((answer) => (
+					{quizzes[currentNo].answers.map((answer) => (
 						<button
 							value={answer.text}
 							onClick={() => handleClick(answer.isCorrect)}
