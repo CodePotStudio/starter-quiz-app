eb654e46c61886e1135a6492b0ea97e86eb2f477
toycrane <alwaysfun2183@gmail.com>
2021-03-02 12:06:15 +0900

    3회차 코드 수정

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,26 +1,10 @@
+// components/Button/index.js
 import React from "react";
-import styled from "styled-components";
-
-const StyledButton = styled.button`
-  font-size: ${(props) => (props.fontSize === "big" ? "32px" : "16px")};
-	color: #ffffff;
-  background-color: ${(props) => props.theme.primaryColor100};
-	border-radius: 5px;
-	border: 0px;
-	height: 56px;
-	padding: 4px;
-	margin: 4px;
-	cursor: pointer;
-	width: 100%;
-	outline: none;
-	font-weight: 700;
-	&:hover {
-    background-color: ${(props) => props.theme.primaryColor80};
-	}
-}
-`;
+import "./Button.css";
 
 const Button = (props) => (
-	<StyledButton onClick={props.onClick}>{props.text}</StyledButton>
+	<button className="answer" onClick={props.onClick}>
+		{props.text}
+	</button>
 );
 export default Button;
