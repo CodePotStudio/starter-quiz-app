1110d4005cf3722bc02d8f5b0ca6af98c94cbc3b
toycrane <alwaysfun2183@gmail.com>
2021-03-08 23:35:23 +0900

    history 추가

--- src/pages/Quiz/index.js
+++ src/pages/Quiz/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useHistory } from "react-router";
 import AnswerGroup from "../../components/AnswerGroup";
 import Container from "../../components/Container";
 import QuestionSection from "../../components/QuestionSection";
@@ -6,13 +7,14 @@ import { QUIZZES } from "../../constants";
 
 const Quiz = ({ setScore }) => {
 	const [currentNo, setCurrentNo] = useState(0);
+	let history = useHistory();
 
 	const handleClick = (isCorrect) => {
 		if (isCorrect) {
 			setScore((score) => score + 1);
 		}
-		// 마지막 퀴즈인지 체크하기
 		if (currentNo === QUIZZES.length - 1) {
+			history.push("/result");
 		} else {
 			setCurrentNo((currentNo) => currentNo + 1);
 		}
