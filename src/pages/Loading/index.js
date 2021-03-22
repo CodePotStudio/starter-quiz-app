7b3ff7dd36a5e19727e63f6841c7aa2ea1f159c6
toycrane <alwaysfun2183@gmail.com>
2021-03-23 01:36:31 +0900

    clearTimeout 추가

--- src/pages/Loading/index.js
+++ src/pages/Loading/index.js
@@ -1,4 +1,6 @@
 import { Container, Spinner } from "components";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 import styled from "styled-components";
 
 const Title = styled.h1`
@@ -10,9 +12,23 @@ const Title = styled.h1`
 `;
 
 const Loading = () => {
+	const [title, setTitle] = useState("결과 분석중");
+	let history = useHistory();
+
+	useEffect(() => {
+		const id = setTimeout(() => {
+			setTitle((title) => title + ".");
+		}, 700);
+		return () => clearTimeout(id);
+	}, [title]);
+
+	useEffect(() => {
+		setTimeout(() => history.push("/result"), 2000);
+	}, [history]);
+
 	return (
 		<Container>
-			<Title>결과 분석중</Title>
+			<Title>{title}</Title>
 			<Spinner></Spinner>
 		</Container>
 	);
