f183c319c88fc38e3965b7f042f6510ca4e8a5c3
toycrane <alwaysfun2183@gmail.com>
2021-03-30 15:36:39 +0900

    twitter, facebook 버튼 추가

--- src/components/SocialButtonGroup/index.js
+++ src/components/SocialButtonGroup/index.js
@@ -1,4 +1,6 @@
 import styled from "styled-components";
+import { FacebookShareButton, FacebookIcon } from "react-share";
+import { TwitterShareButton, TwitterIcon } from "react-share";
 
 // 버튼별로 40px을 확보하고, 각 영역 간에는 8px의 간격을 둠.
 const GridContainer = styled.div`
@@ -10,10 +12,16 @@ const GridContainer = styled.div`
 `;
 
 const SocialButtonGroup = () => {
+	const currentUrl = window.location.href;
+
 	return (
 		<GridContainer>
-			<button>1</button>
-			<button>2</button>
+			<FacebookShareButton url={currentUrl}>
+				<FacebookIcon size={48} round={true} borderRadius={24}></FacebookIcon>
+			</FacebookShareButton>
+			<TwitterShareButton url={currentUrl}>
+				<TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
+			</TwitterShareButton>
 			<button>3</button>
 			<button>4</button>
 		</GridContainer>
