63aa1a5e6377669f0a355eb35bd17e7dd22509bd
toycrane <alwaysfun2183@gmail.com>
2021-03-30 15:44:37 +0900

    url 복사 버튼 추가

--- src/components/SocialButtonGroup/index.js
+++ src/components/SocialButtonGroup/index.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { FacebookShareButton, FacebookIcon } from "react-share";
 import { TwitterShareButton, TwitterIcon } from "react-share";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 // 버튼별로 40px을 확보하고, 각 영역 간에는 8px의 간격을 둠.
 const GridContainer = styled.div`
@@ -9,6 +10,21 @@ const GridContainer = styled.div`
 	grid-column-gap: 8px;
 	justify-content: center;
 	margin-bottom: 16px;
+`;
+
+const URLShareButton = styled.button`
+	width: 48px;
+	height: 48px;
+	color: white;
+	border-radius: 24px;
+	border: 0px;
+	font-weight: 800;
+	font-size: 18px;
+	cursor: pointer;
+	background-color: ${(props) => props.theme.primaryColor100};
+	&:hover {
+		background-color: ${(props) => props.theme.primaryColor80};
+	}
 `;
 
 const SocialButtonGroup = () => {
@@ -22,7 +38,9 @@ const SocialButtonGroup = () => {
 			<TwitterShareButton url={currentUrl}>
 				<TwitterIcon size={48} round={true} borderRadius={24}></TwitterIcon>
 			</TwitterShareButton>
-			<button>3</button>
+			<CopyToClipboard text={currentUrl}>
+				<URLShareButton>URL</URLShareButton>
+			</CopyToClipboard>
 			<button>4</button>
 		</GridContainer>
 	);
