535baaf3f49b5ebf56db0778aadf872489014586
toycrane <alwaysfun2183@gmail.com>
2021-04-05 16:41:34 +0900

    meta tag 테스트

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import { Quiz, Landing, Result, Loading } from "pages";
 import { QUIZZES } from "../constants";
 import { Helmet } from "react-helmet";
 import favicon from "assets/images/favicon.ico";
+import elon from "assets/images/elon.jpg";
 
 function App() {
 	const [score, setScore] = useState(0);
@@ -17,6 +18,9 @@ function App() {
 			<Helmet>
 				<title>일론 머스크 지수 테스트하기</title>
 				<link rel="icon" href={favicon} />
+				<meta property="og:title" content="Hello" />
+				<meta property="og:description" content="Helllllo!" />
+				<meta property="og:image" content={elon} />
 			</Helmet>
 			<GlobalStyle />
 			<Router>
