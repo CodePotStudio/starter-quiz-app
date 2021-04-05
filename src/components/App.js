b5e933ed9147ac441fdd7763586b94745fc556fa
toycrane <alwaysfun2183@gmail.com>
2021-04-05 17:09:03 +0900

    메타 태그 추가하기

--- src/components/App.js
+++ src/components/App.js
@@ -7,10 +7,12 @@ import { Quiz, Landing, Result, Loading } from "pages";
 import { QUIZZES } from "../constants";
 import { Helmet } from "react-helmet";
 import favicon from "assets/images/favicon.ico";
+import elon from "assets/images/elon.jpg";
 
 function App() {
 	const [score, setScore] = useState(0);
 	const convertedScore = Math.floor((score / QUIZZES.length) * 100);
+	const currentUrl = window.location.href;
 
 	return (
 		<ThemeProvider theme={theme}>
@@ -18,6 +20,13 @@ function App() {
 				<title>일론 머스크 지수 테스트하기</title>
 				<link rel="icon" href={favicon} />
 				<meta property="og:title" content="Hello" />
+				<meta property="og:url" content={currentUrl} />
+				<meta property="og:title" content="일론 머스크 지수 테스트하기" />
+				<meta
+					property="og:description"
+					content="나는 일론 머스크에 대해서 얼마나 알고 있을까?"
+				/>
+				<meta property="og:image" content={elon} />
 			</Helmet>
 			<GlobalStyle />
 			<Router>
