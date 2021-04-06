f37c03447aa70c74b54511f0b5818b758a126ddf
toycrane <alwaysfun2183@gmail.com>
2021-04-06 20:43:07 +0900

    메타 데이터 추가

--- src/components/App.js
+++ src/components/App.js
@@ -7,16 +7,37 @@ import { Quiz, Landing, Result, Loading } from "pages";
 import { QUIZZES } from "../constants";
 import { Helmet } from "react-helmet";
 import favicon from "assets/images/favicon.ico";
+import elon from "assets/images/elon.jpg";
 
 function App() {
 	const [score, setScore] = useState(0);
 	const convertedScore = Math.floor((score / QUIZZES.length) * 100);
+	const currentUrl = document.location.href;
 
 	return (
 		<ThemeProvider theme={theme}>
 			<Helmet>
 				<title>일론 머스크 지수 테스트하기</title>
 				<link rel="icon" href={favicon} />
+				{/* URL 정보 */}
+				<meta property="og:url" content={currentUrl} />
+				{/* title 정보 */}
+				<meta property="og:title" content="일론 머스크 지수 테스트하기" />
+				{/* 페이지 상세 정보 */}
+				<meta
+					property="og:description"
+					content="나는 일론 머스크에 대해서 얼마나 알고 있을까?"
+				/>
+				{/* 페이지 대표 이미지 정보 */}
+				<meta property="og:image" content={elon} />
+
+				{/* 트위터 메타 정보 */}
+				<meta name="twitter:title" content="일론 머스크 지수 테스트하기" />
+				<meta
+					name="twitter:description"
+					content="나는 일론 머스크에 대해서 얼마나 알고 있을까?"
+				/>
+				<meta name="twitter:image" content={elon} />
 			</Helmet>
 			<GlobalStyle />
 			<Router>
