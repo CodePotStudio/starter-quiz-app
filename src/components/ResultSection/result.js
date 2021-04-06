59e8eb72f7f49b2b783af0bc266dda04db757542
toycrane <alwaysfun2183@gmail.com>
2021-04-06 11:06:01 +0900

    동적 결과 페이지 추가

--- src/components/ResultSection/result.js
+++ src/components/ResultSection/result.js
@@ -7,18 +7,30 @@ const RESULT = {
 	elon: {
 		title: "일론 머스크",
 		src: elon,
+		ogTitle: "나는 아마도 일론 머스크?",
+		ogDescription: "일론 머스크의 모든 것을 아는 나",
+		ogImage: elon,
 	},
 	kimbal: {
 		title: "킴벌 머스크",
 		src: kimbal,
+		ogTitle: "나는 아마도 일론 머스크의 동생?",
+		ogDescription: "일론 머스크에 대해 이 정도까지 안다면 동생이나 다름없죠",
+		ogImage: kimbal,
 	},
 	bezos: {
 		title: "제프 베조스",
 		src: bezos,
+		ogTitle: "나는 아마도 일론 머스크의 천적?",
+		ogDescription: "여러모로 아직 많이 부족한 것 같아요!",
+		ogImage: bezos,
 	},
 	timemachine: {
 		title: "과거에서 온 사람",
 		src: timemachine,
+		ogTitle: "혹시 과거에서 오셨나요??",
+		ogDescription: "실존하는 아이언맨에 대해 많이 모르시는 것 같아 아쉽습니다.",
+		ogImage: timemachine,
 	},
 };
 
--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -5,17 +5,43 @@ import {
 	SocialButtonGroup,
 } from "components";
 import { useParams } from "react-router";
+import { Helmet } from "react-helmet";
+import elon from "assets/images/elon.jpg";
+import RESULT from "components/ResultSection/result";
 
 const Result = ({ setScore }) => {
 	const { cCode } = useParams();
+	const currentUrl = window.document.href;
 	return (
-		<Container>
-			<ResultSection cCode={cCode}></ResultSection>
-			<SocialButtonGroup></SocialButtonGroup>
-			<Button onClick={() => setScore(0)} to="/">
-				테스트 다시하기
-			</Button>
-		</Container>
+		<>
+			<Helmet>
+				{/* URL 정보 */}
+				<meta property="og:url" content={currentUrl} />
+				{/* title 정보 */}
+				<meta property="og:title" content={RESULT[cCode]["ogTitle"]} />
+				{/* 페이지 상세 정보 */}
+				<meta
+					property="og:description"
+					content={RESULT[cCode]["ogDescription"]}
+				/>
+				{/* 페이지 대표 이미지 정보 */}
+				<meta property="og:image" content={RESULT[cCode]["ogImage"]} />
+				{/* 트위터 메타 정보 */}
+				<meta name="twitter:title" content={RESULT[cCode]["ogTitle"]} />
+				<meta
+					name="twitter:description"
+					content={RESULT[cCode]["ogDescription"]}
+				/>
+				<meta name="twitter:image" content={RESULT[cCode]["ogImage"]} />
+			</Helmet>
+			<Container>
+				<ResultSection cCode={cCode}></ResultSection>
+				<SocialButtonGroup></SocialButtonGroup>
+				<Button onClick={() => setScore(0)} to="/">
+					테스트 다시하기
+				</Button>
+			</Container>
+		</>
 	);
 };
 
